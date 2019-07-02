fbe8435c52f43d5dff51e06548212f87723d2184
SPavelV <pashius2010@gmail.com>
2019-07-02 11:36:40 +0400

    add error indicator during data loadint

--- src/components/random-planet/random-planet.js
+++ src/components/random-planet/random-planet.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 
 import SwapiService from '../../services/swapi-service'
 import Spinner from '../spinner/'
+import ErrorIndicator from '../error-indicator/'
 
 import './random-planet.css'
 
@@ -11,7 +12,8 @@ export default class RandomPlanet extends Component {
 
   state = {
     planet: {},
-    loading: true
+    loading: true,
+    error: false
   }
 
   constructor() {
@@ -22,24 +24,39 @@ export default class RandomPlanet extends Component {
   onPlanetLoaded = (planet) => {
     this.setState({
       planet,
+      loading: false,
+      error: false
+    })
+  }
+
+  onError = (err) => {
+    this.setState({
+      error: true,
       loading: false
     })
   }
 
   updatePlanet() {
     const id = Math.floor(Math.random()*20) + 2;
+    // const id = 200000
     this.swapiService
       .getPlanet(id)
       .then(this.onPlanetLoaded)
+      .catch(this.onError)
   }
 
   render() {
-    const {loading, planet} = this.state
+    const {loading, planet, error} = this.state
+
+    const hasData = !(loading || error)
+
+    const errorMessage = error ? <ErrorIndicator/> : null
     const spinner  = loading ? <Spinner/> : null
-    const content = !loading ? <PlanetView planet={planet}/> : null
+    const content = hasData ? <PlanetView planet={planet}/> : null
     
     return (
       <div className="random-planet jumbotron rounded">
+        {errorMessage}
         {spinner}
         {content}
       </div>

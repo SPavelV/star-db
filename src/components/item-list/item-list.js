763c5cdca9c6c1b9479be5980bce73f85ecbe1d9
SPavelV <pashius2010@gmail.com>
2019-07-04 10:42:45 +0400

    update loading data for item-list component

--- src/components/item-list/item-list.js
+++ src/components/item-list/item-list.js
@@ -1,22 +1,68 @@
 import React, { Component } from 'react';
 
+import SwapiService from '../../services/swapi-service'
+import Spinner from '../spinner'
+import ErrorIndicator from '../error-indicator/'
 import './item-list.css';
 
 export default class ItemList extends Component {
+  
+  swapiService = new SwapiService()
+
+  state = {
+    peopleList: null,
+    lading: true,
+    error: false
+  }
+
+  componentDidMount() {
+    this.swapiService
+      .getAllPeople()
+      .then(this.onPeopleListLoaded)
+      // .catch(this.onError)
+      
+  }
+
+  onPeopleListLoaded = (peopleList) => {
+    this.setState({
+      peopleList,
+      loading: false,
+      error: false
+    })
+  }
+
+  onError = (err) => {
+    this.setState({
+      error: true,
+      loading: false
+    })
+  }
+
+  renderItems(arr) {
+    return arr.map(({id, name}) => {
+      return (
+        <li className="list-group-item"
+            key={id}
+            >
+          {name}
+        </li>
+      )
+    })
+  }
 
   render() {
+    const { peopleList } = this.state;
+
+    if(!peopleList) {
+      return <Spinner/>
+    }
+
+    const items = this.renderItems(peopleList)
+
     return (
       <ul className="item-list list-group">
-        <li className="list-group-item">
-          Luke Skywalker
-        </li>
-        <li className="list-group-item">
-          Darth Vader
-        </li>
-        <li className="list-group-item">
-          R2-D2
-        </li>
+        {items}
       </ul>
-    );
+    )
   }
 }
